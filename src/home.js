105a8931a640ef7ed69ea3c36737caf750e2dff1
Carlos Ospina <carlossospinag@gmail.com>
2021-05-28 17:25:35 -0500

    indent home.js

--- src/home.js
+++ src/home.js
@@ -4,27 +4,27 @@ const renderHomePage = (() => {
   const homeHtml = `
     <main>
     <h1 class="text-center"> Restaurant Page </h1>
-<div class="card text-white bg-dark mb-3 mx-auto" style="max-width: 18rem;">
-<div class="card-body">
+  <div class="card text-white bg-dark mb-3 mx-auto" style="max-width: 18rem;">
+  <div class="card-body">
   <h5 class="card-title">Delicious FastFood</h5>
   <p class="card-text">Prepare yourself to taste the heaven in a dish.</p>
-</div>
-</div>
-<div class="card text-white bg-dark mb-3 mx-auto" style="max-width: 18rem;">
-<div class="card-header">Hours</div>
-<div class="card-body">
+  </div>
+  </div>
+  <div class="card text-white bg-dark mb-3 mx-auto" style="max-width: 18rem;">
+  <div class="card-header">Hours</div>
+  <div class="card-body">
   <h5 class="card-title">24/7</h5>
   <p class="card-text"> From Thursday to Sunday only.</p>
-</div>
-</div>
-<div class="card text-white bg-dark mb-3 mx-auto" style="max-width: 18rem;">
-<div class="card-header">Location</div>
-<div class="card-body">
+  </div>
+  </div>
+  <div class="card text-white bg-dark mb-3 mx-auto" style="max-width: 18rem;">
+  <div class="card-header">Location</div>
+  <div class="card-body">
   <h5 class="card-title">Barranquilla, Colombia</h5>
   <p class="card-text">742 Evergreen Terrace.</p>
-</div>
-</div>
-    </main>`
+  </div>
+  </div>
+  </main>`
   contentContainer.innerHTML = homeHtml;
 
   const homeLink = document.querySelector('#home-link')
