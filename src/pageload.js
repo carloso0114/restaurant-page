07c89fc4710dd921e751332d8417ed3d400be07e
Carlos Ospina <carlossospinag@gmail.com>
2021-05-31 16:06:55 -0500

    fix nav bar

--- src/pageload.js
+++ src/pageload.js
@@ -1,32 +1,28 @@
 const pageLoad = (() => {
   const contentContainer = document.querySelector('body');
-  
   const header = document.createElement('header');
   header.innerHTML = `
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-
-      <div class="collapse navbar-collapse justify-content-md-center">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#" id="home-link">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" id="menu-link">Menu</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" id="contact-link">Contact Us</a>
-          </li>
-        </ul>
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarsExample02">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#" id="home-link">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" id="menu-link">Menu</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" id="contact-link">Contact Us</a>
+        </li>
+      </ul>
       </div>
-
-    </div>
-  </nav>`;
+  </div>
+  </nav>
+  `;
   
   const content = document.createElement('div');
   content.setAttribute('id', 'content');
-
-
 
   const footer = document.createElement('footer');
 
